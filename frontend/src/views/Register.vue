13ea8fe8a18a7d82a4a7b3f7349eb2047b9ad6d3
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-05-13 23:42:58 +0900

    [ENH] 회원가입 페이지 디자인 추가

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,11 +1,43 @@
 <template>
-    <div>
+    <article id="Register" class="account-form">
+        <h1>Register</h1>
 
-    </div>
+        <form @submit.prevent="register">
+            <div class="form-group">
+                <input type="text" placeholder="ID" v-model="user.id">
+            </div>
+            <div class="form-group">
+                <input type="text" placeholder="Name" v-model="user.name">
+            </div>
+            <div class="form-group">
+                <input type="password" placeholder="Password" v-model="user.password">
+            </div>
+
+            <div class="btn-wrap">
+                <input type="submit" class="btn-submit" value="Register">
+                <router-link class="btn-link"
+                    to="/login">back</router-link>
+            </div>
+        </form>
+    </article>
 </template>
 
 <script>
 export default {
-    name: 'Register'
+    name: 'Register',
+    data () {
+        return {
+            user: {
+                id: '',
+                name: '',
+                password: ''
+            }
+        }
+    },
+    methods: {
+        register () {
+            console.log('Register');
+        }
+    }
 }
 </script>
