324ef6163bc9a3263c397d15f0ab161dbe3fa7de
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-05-14 00:01:57 +0900

    [ENH] 메인 페이지 디자인 추가

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,22 @@
 <template>
-    <div>
+    <article id="home" class="home-container">
+        <h1>User Info</h1>
 
-    </div>
+        <div class="info-wrap">
+            <div class="info-group">
+                <h2>Login ID</h2>
+                <p>test1234</p>
+            </div>
+            <div class="info-group">
+                <h2>Name</h2>
+                <p>user name</p>
+            </div>
+        </div>
+
+        <div class="btn-wrap">
+            <a href="#" class="btn-logout">Logout</a>
+        </div>
+    </article>
 </template>
 
 <script>
