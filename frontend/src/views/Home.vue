4a77c05512fc15c5646184df8f5bd58f7779b223
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-05-20 23:30:12 +0900

    [ENH] 유저 정보 출력 페이지 작업

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -5,16 +5,24 @@
         <div class="info-wrap">
             <div class="info-group">
                 <h2>Login ID</h2>
-                <p>test1234</p>
+                <p v-if="user">
+                    {{ user.login_id }}
+                </p>
             </div>
             <div class="info-group">
                 <h2>Name</h2>
-                <p>user name</p>
+                <p v-if="user">
+                    {{ user.name }}
+                </p>
             </div>
         </div>
 
         <div class="btn-wrap">
-            <a href="#" class="btn-logout">Logout</a>
+            <a href="#" class="btn-logout" v-if="user">Logout</a>
+            <router-link to="/login" class="btn-logout"
+                         v-else>
+                Login
+            </router-link>
         </div>
     </article>
 </template>
@@ -22,6 +30,21 @@
 <script>
 
 export default {
-  name: 'Home',
+    name: 'Home',
+    data () {
+        return {
+            user: ''
+        }
+    },
+    mounted () {
+        this.user = this.getUser
+    },
+    computed: {
+        getUser () {
+            if (this.$store.state.token) {
+                return JSON.parse(atob(this.$store.state.token.split('.')[1]));
+            }
+        }
+    }
 }
 </script>
